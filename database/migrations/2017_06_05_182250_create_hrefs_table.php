be3ad0472f46322043406a3fbacae09b09cb102b
Igors Krasjukovs <tahq69@gmail.com>
2017-06-05 21:34:56 +0300

    DB structure added;

---
+++ database/migrations/2017_06_05_182250_create_hrefs_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class CreateHrefsTable
+ */
+class CreateHrefsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hrefs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('parent_id');
+
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamp('date_added')->nullable();
+            $table->unsignedInteger('index')->default(0);
+            $table->boolean('visible')->default(true);
+
+            $table->string('title');
+            $table->string('url', 500)->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hrefs');
+    }
+}
